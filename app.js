2f666f7b1e40d037a13a6979b68b4eea0d5807b4
Milad.A <millad.99.awad@gmail.com>
2020-12-31 11:31:38 +0200

    Add Database configuration

---
+++ app.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv')
+const express = require('express')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
+
+// Load config
+dotenv.config({
+    path: './config/config.env',
+})
+
+connectDB()
+
+const app = express()
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+)
