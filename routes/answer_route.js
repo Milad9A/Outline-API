4b294d30abc490ae47a0d8c582a74a140179fad8
Milad.A <millad.99.awad@gmail.com>
2021-08-28 01:40:00 +0300

    Add answer vote route

--- routes/answer_route.js
+++ routes/answer_route.js
@@ -12,6 +12,8 @@ router.get('/answers/me', auth, AnswerController.getMyAnswers)
 
 router.get('/answers/:id', auth, AnswerController.getAnswer)
 
+router.post('/answers/:id/vote/', auth, AnswerController.voteAnswer)
+
 router.patch('/answers/:id', auth, AnswerController.updateAnswer)
 
 router.delete('/answers/:id', auth, AnswerController.deleteAnswer)
